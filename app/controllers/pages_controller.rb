dbbc639da011df51db11afa5bbdecd8366f3b044
Bodil Hundevad <bodil@hundevad.de>
2023-03-01 13:55:49 +0100

    Refactor: Remove Base64 requirement from PagesController
    - Removed unnecessary `require "base64"`
    - Updated document to reflect changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
-require "base64"
 require "open-uri"
 class PagesController < ApplicationController
   def home
     client = OpenAI::Client.new
-      #imageEncoded = Base64.encode64(open(asset_url) { |f| f.read })
     response = client.images.generate(parameters: {
       prompt: "Create a funny cute childrens illustrated storybook image: Once upon a time, there was a curious otter named Olive who loved to swim.",
     })
